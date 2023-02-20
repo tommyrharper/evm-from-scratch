cc245636fdf30a0de91db676b70eac1d5345d6e6
tommyrharper <thomasrobertharper@gmail.com>
2023-02-20 17:41:02 +0000

    feat: add PUSH

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,7 +6,7 @@ pub struct EvmResult {
 }
 
 pub fn evm(_code: impl AsRef<[u8]>) -> EvmResult {
-    let stack: Vec<U256> = Vec::new();
+    let mut stack: Vec<U256> = Vec::new();
     let mut pc = 0;
 
     let code = _code.as_ref();
@@ -14,9 +14,21 @@ pub fn evm(_code: impl AsRef<[u8]>) -> EvmResult {
     while pc < code.len() {
         let opcode = code[pc];
         pc += 1;
-
+        println!("-------pc {:?}", pc);
+        println!("-------code {:?}", code);
+        println!("-------opcode {:?}", opcode);
+        println!("-------stack {:?}", stack);
         if opcode == 0x00 {
             // STOP
+            break;
+        } else if opcode == 0x60 {
+            // PUSH
+            // stack.push(U256::from(0x1));
+            stack.push(U256::from(code[pc]));
+            return EvmResult {
+                stack: stack,
+                success: true,
+            };
         }
     }
 
