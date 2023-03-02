db83b52bc768994143bfc997f576b1f2682f84f8
tommyrharper <thomasrobertharper@gmail.com>
2023-03-02 16:10:51 +0000

    refactor: add <16 check in hexadecimal_concat map

--- rust/src/helpers.rs
+++ rust/src/helpers.rs
@@ -2,17 +2,16 @@ use primitive_types::U256;
 
 pub fn concat_decimals(arr: &[u8]) -> U256 {
     let hexadecimal_concat: String = arr
-    .iter()
-    .map(|x| {
-        // TODO: update to use num check instead of str len
-        let mut hex = format!("{:X}", x);
-        if hex.len() == 1 {
-            hex = format!("0{:X}", x);
-        }
-        return hex;
-    })
-    .collect::<Vec<String>>()
-    .join("");
+        .iter()
+        .map(|x| {
+            if x < &16 {
+                format!("0{:X}", x)
+            } else {
+                format!("{:X}", x)
+            }
+        })
+        .collect::<Vec<String>>()
+        .join("");
 
     return U256::from_str_radix(&hexadecimal_concat, 16).unwrap();
 }
