73bd9a976376c5170d528c683c35273e7a9e6c80
tommyrharper <thomasrobertharper@gmail.com>
2023-03-03 14:43:06 +0000

    feat: set fn in Stack

--- rust/src/stack.rs
+++ rust/src/stack.rs
@@ -18,6 +18,15 @@ impl Stack {
         self.data.pop()
     }
 
+    pub fn set(&mut self, value: U256, i: usize) -> Result<(), EvmError> {
+        let len = self.data.len();
+        if len > i {
+            self.data[len - i - 1] = value;
+            Ok(())
+        } else {
+            Err(EvmError::StackUnderflow)
+        }
+    }
     pub fn peek(&self, i: usize) -> Result<U256, EvmError> {
         if self.data.len() > i {
             Ok(self.data[self.data.len() - i - 1])
