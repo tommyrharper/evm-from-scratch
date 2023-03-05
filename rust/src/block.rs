597fb42c5d8cd59a36ade0aaaaaa6e07b7e0516c
tommyrharper <thomasrobertharper@gmail.com>
2023-03-05 21:24:13 +0000

    refactor: basefee to last arg Block

--- rust/src/block.rs
+++ rust/src/block.rs
@@ -1,22 +1,22 @@
 pub struct Block<'a> {
-    pub basefee: &'a [u8],
     pub coinbase: &'a [u8],
     pub timestamp: &'a [u8],
     pub number: &'a [u8],
     pub difficulty: &'a [u8],
     pub gaslimit: &'a [u8],
     pub chainid: &'a [u8],
+    pub basefee: &'a [u8],
 }
 
 impl<'a> Block<'a> {
     pub fn new(
-        basefee: &'a [u8],
         coinbase: &'a [u8],
         timestamp: &'a [u8],
         number: &'a [u8],
         difficulty: &'a [u8],
         gaslimit: &'a [u8],
         chainid: &'a [u8],
+        basefee: &'a [u8],
     ) -> Self {
         Self {
             basefee,
