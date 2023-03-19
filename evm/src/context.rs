bedf2ab083c01a73bf015c244cd8b2315cd2d6c1
tommyrharper <thomasrobertharper@gmail.com>
2023-03-19 12:23:59 +0000

    refactor: update data to call_data in Context

--- evm/src/context.rs
+++ evm/src/context.rs
@@ -8,8 +8,7 @@ pub struct Context<'a> {
     pub gasprice: U256,
     // TODO: update to U256, handle overlap with State.Account.balance -> maybe not
     pub value: U256,
-    // TODO: update this to be call_data for clarity
-    pub data: &'a String,
+    pub call_data: &'a String,
     pub state: State,
     pub is_static: bool,
 }
@@ -23,7 +22,7 @@ impl<'a> Context<'a> {
         gasprice: U256,
         value: U256,
         // TODO: update data to call_data and store as hex not string
-        data: &'a String,
+        call_data: &'a String,
         state: State,
         is_static: bool,
     ) -> Self {
@@ -33,25 +32,25 @@ impl<'a> Context<'a> {
             origin,
             gasprice,
             value,
-            data,
+            call_data,
             state,
             is_static,
         }
     }
 
     pub fn calldata_size(&self) -> U256 {
-        let data = hex::decode(&self.data).unwrap().len();
-        data.into()
+        let call_data_size = hex::decode(&self.call_data).unwrap().len();
+        call_data_size.into()
     }
 
     pub fn load_calldata(&self, byte_offset: usize, target_size: usize) -> U256 {
-        let data = hex::decode(&self.data).unwrap();
+        let call_data = hex::decode(&self.call_data).unwrap();
         let mut res: Vec<u8> = vec![0; target_size];
 
         for i in 0..target_size {
             let data_index = i + byte_offset;
-            if data_index < data.len() {
-                let val = data[data_index];
+            if data_index < call_data.len() {
+                let val = call_data[data_index];
                 res[i] = val;
             }
         }
